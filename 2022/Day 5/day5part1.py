18094d7268b7fbe70391852324b3cf53aba1f785
AliDee <ali.dee1410@gmail.com>
2022-12-05 12:48:50 +0000

    Day 5

---
+++ 2022/Day 5/day5part1.py
@@ -0,0 +1,52 @@
+def read_input():
+    stacks = {}
+    steps = []
+    with open("day5input.txt", "r") as file:
+        # Stacks
+        line = file.readline()
+        while line != "\n":
+            row = line.strip("\n")
+            for i in range(0, len(row), 4):
+                if row[i] == "[":
+                    stack = int(i / 4) + 1
+                    crate = row[i + 1]
+                    if stack in stacks:
+                        # Crates are added from top to bottom. This is wrong and will be handled later...
+                        stacks[stack].append(crate)
+                    else:
+                        stacks[stack] = [crate]
+            line = file.readline()
+
+        # Reverse the stacks, as the crates were added from top to bottom, not bottom to top
+        for stack in stacks:
+            stacks[stack].reverse()
+
+        # Steps
+        line = file.readline()
+        while line:
+            if line != "\n":
+                step =[]
+                for x in line.strip("\n").replace("move ", "").replace("from ", "").replace("to ", "").split(" "):
+                    step.append(int(x))
+                steps.append(step)
+            line = file.readline()
+    return stacks, steps
+
+
+stacks, steps = read_input()
+
+# Carry out procedure steps
+for step in steps:
+    num_crates = step[0]
+    from_stack = step[1]
+    to_stack = step[2]
+    for _ in range(num_crates):
+        stacks[to_stack].append(stacks[from_stack].pop())
+
+# Print final stacks and final message
+message = ""
+for stack in sorted(stacks):
+    print("Stack {}: {}".format(stack, stacks[stack]))
+    if stacks[stack]:
+        message += stacks[stack].pop()
+print(message)
