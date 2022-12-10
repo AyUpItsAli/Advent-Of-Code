47b7358e723165a2a5608babcd1de8080415806a
Ali <ali.dee1410@gmail.com>
2022-12-10 10:06:01 +0000

    Day 10 Part 2

--- 2022/Day 10/day10.py
+++ 2022/Day 10/day10.py
@@ -12,38 +12,63 @@ def read_input():
     return out
 
 
+CRT_HEIGHT = 6
+CRT_WIDTH = 40
+SPRITE_WIDTH = 3
+BLANK = " "
+DARK = "."
+LIT = "#"
+
+
+def setup_crt_screen():
+    screen = []
+    for y in range(CRT_HEIGHT):
+        row = []
+        for x in range(CRT_WIDTH):
+            row.append(BLANK)
+        screen.append(row)
+    return screen
+
+
 NOOP = "noop"
-ADDX = "addx"
 PROCESS_ADDX = "process addx"
+ADDX = "addx"
 
 
 operations = read_input()
 op_queue = []
 X = 1
 cycle = 0
+crt_screen = setup_crt_screen()
+draw_x_pos = 0
+draw_y_pos = 0
 
 
 def add_next_operation():
     if not operations:
         return
-    next_op = operations.pop(0)
-    if next_op[0] == NOOP:
-        op_queue.append(next_op)
-    elif next_op[0] == ADDX:
-        process = [PROCESS_ADDX]
-        op_queue.extend([process, next_op])
+    op = operations.pop(0)
+    if op[0] == NOOP:
+        op_queue.append(op)
+    elif op[0] == ADDX:
+        process_op = [PROCESS_ADDX]
+        op_queue.extend([process_op, op])
 
 
 def process_next_operation():
     global X
-
     if not op_queue:
         return
-    next_op = op_queue.pop(0)
-    if next_op[0] == ADDX:
-        value = next_op[1]
+    op = op_queue.pop(0)
+    if op[0] == ADDX:
+        value = op[1]
         X += value
-    return next_op
+        #print("ADDX {}, X = {}".format(value, X))
+    return op
+
+
+def draw_next_pos():
+    crt_screen[draw_y_pos][draw_x_pos] = LIT if draw_x_pos in [X - 1, X, X + 1] else DARK
 
 
 START = 20
@@ -54,10 +79,26 @@ add_next_operation()
 while op_queue:
     # Start of cycle
     cycle += 1
+    #print("Cycle: {}, Queue: {}".format(cycle, op_queue))
     # During cycle
     if (cycle + START) % EVERY == 0:
         signal_sum += X * cycle
+    #print("Drawing: {}, {}".format(draw_x_pos, draw_y_pos))
+    draw_next_pos()
+    #print(crt_screen[draw_y_pos])
     # End of cycle
-    next_op = process_next_operation()
+    process_next_operation()
+    #print("Sprite pos: {}, {}".format(X, draw_y_pos))
     add_next_operation()
-print(signal_sum)
+    draw_x_pos += 1
+    if draw_x_pos % CRT_WIDTH == 0:
+        draw_x_pos = 0  # Reset X pos
+        draw_y_pos += 1  # Increase Y pos
+    #print()
+#print(signal_sum)
+
+for row in crt_screen:
+    string = ""
+    for x in row:
+        string += x
+    print(string)
