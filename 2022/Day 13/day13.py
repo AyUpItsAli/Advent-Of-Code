06d827f85f2dc5b308c7cd39d994334aebffad1c
Ali <ali.dee1410@gmail.com>
2022-12-13 10:06:19 +0000

    Day 13 Part 2

--- 2022/Day 13/day13.py
+++ 2022/Day 13/day13.py
@@ -37,9 +37,38 @@ def compare_list(listLeft: list, listRight: list):
     return len(listLeft) < len(listRight)
 
 
+# Part 1
 pairs = read_input()
 sum_indices = 0
 for i, pair in enumerate(pairs):
     if compare_list(pair[0], pair[1]):
         sum_indices += i + 1
 print(sum_indices)
+
+
+def bubble_sort(packets: list):
+    while True:
+        swapped = False
+        for i in range(len(packets[:-1])):
+            packet = packets[i]
+            next_packet = packets[i + 1]
+            if not compare_list(packet, next_packet):
+                packets[i] = next_packet
+                packets[i + 1] = packet
+                swapped = True
+        if not swapped:
+            break
+
+
+# Part 2
+packets = [packet for pair in pairs for packet in pair]  # Flattens 2D array of pairs into list of packets
+packets.extend([  # Add divider packets
+    [[2]],
+    [[6]]
+])
+bubble_sort(packets)
+decoder_key = 1
+for i, packet in enumerate(packets):
+    if packet == [[2]] or packet == [[6]]:
+        decoder_key *= i + 1
+print(decoder_key)
